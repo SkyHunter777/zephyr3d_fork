ebffbcaf64685f585c47377f4d3754268bc5e59a
SkyFe <527980522@qq.com>
2026-06-11 12:59:11 +0800

    补充场景节点默认命名
    - 新增通用默认节点命名辅助，统一处理资源拖入、内置网格与编辑器创建节点的命名规则\n- 支持拖入 prefab 或模型时，在根节点名为空的情况下默认使用资源文件名\n- 支持创建灯光、相机、粒子、水体、地形、空节点、批处理组与内置网格后自动补充默认名称\n- 保持仅在节点原名为空时兜底命名，不覆盖资源或用户已有名称

--- utility/editor/src/views/sceneview.ts
+++ utility/editor/src/views/sceneview.ts
@@ -69,6 +69,12 @@ import { NodeProxy } from '../helpers/proxy';
 import { clearScriptPropertyAccessorCache } from '../helpers/scriptprops';
 import { getMorphTargetGroupPropertyAccessors } from '../helpers/morphtargetprops';
 import { shapePrimitivePaths, type ShapePrimitiveType } from '../helpers/shapeprimitives';
+import {
+  ensureNodeDefaultName,
+  getDefaultNodeNameFromAssetPath,
+  getDefaultNodeNameFromCtor,
+  getDefaultShapeNodeName
+} from '../helpers/defaultnodename';
 import type { EditTool, EditToolContext } from './edittools/edittool';
 import { createEditTool, isObjectEditable } from './edittools/edittool';
 import { calcHierarchyBoundingBoxWorld } from '../helpers/misc';
@@ -2706,6 +2712,7 @@ export class SceneView extends BaseView<SceneModel, SceneController> {
     );
     const mesh = new Mesh(this.controller.model.scene, shape!, material!);
     mesh.gpuPickable = false;
+    ensureNodeDefaultName(mesh, getDefaultShapeNodeName(shapeCls));
     this._nodeToBePlaced.set(mesh);
     this._shapeToBeAdded = {
       cls: shapeCls
@@ -2723,6 +2730,7 @@ export class SceneView extends BaseView<SceneModel, SceneController> {
     const node = new ctor(this.controller.model.scene);
     node.parent = null;
     node.gpuPickable = false;
+    ensureNodeDefaultName(node, getDefaultNodeNameFromCtor(ctor));
     this._proxy!.createProxy(node);
     this._nodeToBePlaced.set(node);
     this._typeToBePlaced = 'node';
@@ -2741,6 +2749,7 @@ export class SceneView extends BaseView<SceneModel, SceneController> {
       .resourceManager.instantiatePrefab(this.controller.model.scene.rootNode, prefab)
       .then((node) => {
         node!.parent = null;
+        ensureNodeDefaultName(node, getDefaultNodeNameFromAssetPath(prefab));
         node!.iterate((node) => {
           node.gpuPickable = false;
         });
@@ -2765,6 +2774,7 @@ export class SceneView extends BaseView<SceneModel, SceneController> {
       .resourceManager.fetchModel(asset, this.controller.model.scene)
       .then((node) => {
         node.parent = null;
+        ensureNodeDefaultName(node, getDefaultNodeNameFromAssetPath(asset));
         node.iterate((node) => {
           node.gpuPickable = false;
         });
